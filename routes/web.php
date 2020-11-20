756a151cc9995242956de4cb7dc88d1765ffe388
JustinRM104 <justrom104@gmail.com>
2020-11-20 11:01:01 +0100

    controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get', "ApiController@getData");
+Route::post('/post', "ApiController@postData");
